ea7c270dbec9c6519ebe9db06a26dbf9e5f8a513
GTimothee <ava_punksmash@protonmail.com>
2020-01-03 18:15:09 -0500

    array utils

--- dask_io/utils/array_utils.py
+++ dask_io/utils/array_utils.py
@@ -5,23 +5,15 @@ def get_arr_shapes(arr):
 
     Arguments:
     ----------
-        arr: dask array
+    arr: dask array
 
     Returns:
     --------
-        shape: shape of the dask array
-        chunks: shape of one chunk
-        chunk_dims: number of chunks in eah dimension
+    shape: shape of the dask array
+    chunks: shape of one chunk
+    chunk_dims: number of chunks in each dimension
     """
     shape = arr.shape
     chunks = tuple([c[0] for c in arr.chunks])
     chunk_dims = [len(c) for c in arr.chunks]  
     return shape, chunks, chunk_dims
-
-
-def get_dask_array_chunks_shape(dask_array):
-    t = dask_array.chunks
-    cs = list()
-    for tupl in t:
-        cs.append(tupl[0])
-    return tuple(cs
