8ae9417a421b28de710a8b664048a6614d397330
GTimothee <ava_punksmash@protonmail.com>
2020-04-20 16:55:19 -0400

    updated 2d rectangles overlap function to nd

--- dask_io/optimizer/cases/resplit_utils.py
+++ dask_io/optimizer/cases/resplit_utils.py
@@ -1,33 +1,27 @@
-def hypercubes_overlap(rect1, rect2):
-    corn1 = rect1.get_corners()
-    corn2 = rect2.get_corners()
-    vertical_cross, horizontal_cross = (False, False)
+def hypercubes_overlap(hypercube1, hypercube2):
+    uppercorner1, lowercorner1 = hypercube1
+    uppercorner2, lowercorner2 = hypercube2
+    nb_dims = len(uppercorner1)
     
-    if corn1["bot_left"].y > corn2["top_left"].y \
-        or corn2["bot_left"].y > corn1["top_left"].y:
-        return False
-    else:
-        vertical_cross = True
-    
-    if corn1["bot_right"].x < corn2["bot_left"].x \
-        or corn2["bot_right"].x < corn1["bot_left"].x:
-        return False
-    else:
-        horizontal_cross = True
-    
-    if vertical_cross and horizontal_cross:
-        return True
-    else:
-        return False
+    for i in range(nb_dims):
+        if not uppercorner1[i] > lowercorner1[i] and \
+            not uppercorner2[i] > lowercorner1[i]:
+            return False
+
+    return True
+
 
 def get_crossed_outfiles(buffer_index, outfiles):
     pass 
 
+
 def merge_volumes():
     pass
 
+
 def included_in(volume, outfile):
     pass
+
 
 def add_to_array_dict(array_dict, outfile, volume):
     pass
